6844c40c26757956a81f00ade29e967fea240f3e
Aleksei Petrov <peta0982@gmail.com>
2024-01-29 01:18:16 +0500

    implement command interfase entry point

---
+++ src/main/java/ru/petrov/App.java
@@ -0,0 +1,11 @@
+package ru.petrov;
+
+import ru.petrov.consoleApp.WelcomePage;
+
+public class App {
+    public static void main(String[] args) {
+        Initialization.init();
+        WelcomePage.showMenu();
+
+    }
+}
