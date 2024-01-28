49ab03bfe05aff7bc279d05e0f50020989097c5f
Aleksei Petrov <peta0982@gmail.com>
2024-01-29 00:53:22 +0500

    fix equals AbstractNamedEntity

--- src/main/java/ru/petrov/model/AbstractNamedEntity.java
+++ src/main/java/ru/petrov/model/AbstractNamedEntity.java
@@ -20,6 +20,16 @@ public abstract class AbstractNamedEntity extends AbstractEntity {
 
     @Override
     public String toString() {
-        return String.format("%s. UUID: %s. Name: %s.", getClass().getSimpleName(),uuid,name);
+        return String.format("%s. UUID: %s. Name: %s.", getClass().getSimpleName(), uuid, name);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AbstractNamedEntity that = (AbstractNamedEntity) o;
+        return name.equals(that.name) && uuid.equals(that.uuid);
+    }
+
 }
