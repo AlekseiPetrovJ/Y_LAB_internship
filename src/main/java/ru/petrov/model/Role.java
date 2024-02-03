b1c3edd39d11ce216e4c2954346fcbc1dae0842c
Aleksei Petrov <peta0982@gmail.com>
2024-02-03 13:31:48 +0500

    rev. add roleId

--- src/main/java/ru/petrov/model/Role.java
+++ src/main/java/ru/petrov/model/Role.java
@@ -1,6 +1,16 @@
 package ru.petrov.model;
 
 public enum Role {
-    USER,
-    ADMIN
+    ADMIN(1000),
+    USER(1001);
+
+    private final int roleId;
+
+    private Role(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
 }
