21e8ecdb7c96b3fdd56a5c96130a371355c635c5
Aleksei Petrov <peta0982@gmail.com>
2024-02-18 21:44:52 +0500

    refactoring: UserRepositoryTest implement Testcontainers, jdbc -> jdbctemplate.

--- src/test/java/ru/petrov/repository/jdbc/JdbcUserRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package ru.petrov.repository.jdbc;
-
-import ru.petrov.repository.AbstractUserRepositoryTest;
-
-class JdbcUserRepositoryTest extends AbstractUserRepositoryTest {
-    {
-        setUserRepository(new JdbcUserRepository());
-    }
-}
