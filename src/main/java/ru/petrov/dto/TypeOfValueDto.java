384d63ba20f852c5574febee6bc193610063c256
Aleksei Petrov <peta0982@gmail.com>
2024-02-24 21:38:44 +0500

    fix. delete ID in TypeOfValueDto

--- src/main/java/ru/petrov/dto/TypeOfValueDto.java
+++ src/main/java/ru/petrov/dto/TypeOfValueDto.java
@@ -4,22 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
 public class TypeOfValueDto {
-    @Schema(example = "100000")
-    private Integer id;
     @NotEmpty(message = "Name not might be empty")
     @Schema(example = "hot water")
     private String name;
     @NotEmpty(message = "Unit of measurement name not might be empty")
     @Schema(example = "m3")
     private String unitOfMeasurement;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
